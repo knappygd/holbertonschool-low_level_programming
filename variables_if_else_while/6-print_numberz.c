f30d2738d6802b88266c9673fd88086f37b63586
Emilio Damasco <6338@holbertonstudents.com>
2023-02-15 11:15:09 -0800

    autocommit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
