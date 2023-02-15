75bca5947820f1db00175640fb218306a7a0b50a
Emilio Damasco <6338@holbertonstudents.com>
2023-02-15 10:29:09 -0800

    autocommit

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);	
+	}
+	putchar('\n');
+	return (0);
+}
