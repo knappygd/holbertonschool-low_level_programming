29083451ea9d4a7ded8cbfca30a5d2c22ae5acf2
Emilio Damasco <6338@holbertonstudents.com>
2023-02-15 09:51:21 -0800

    autocommit

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		puts("%d is positive");
+	} 
+	else if (n < 0)
+	{
+		puts("%d is negative");
+	}
+	else
+	{
+		puts("%d is zero");
+	}
+	return (0);
+}
