464ea228a58836d31e55f0b6f4deee68aa39a8be
Emilio Damasco <6338@holbertonstudents.com>
2023-02-15 07:20:48 -0800

    added 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void) 
+{
 	char string[] = "\"Programming is like building a multilingual puzzle";
-	puts(string);
+
+	puts (string);
 	return(0);
 }
