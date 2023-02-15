96087a67de5e3bee47f1adbebce559bdb23e42b0
Emilio Damasco <6338@holbertonstudents.com>
2023-02-15 07:33:48 -0800

    added 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 /**
- * this is the desc of the main() function
+ * this is the short desc of the main() function
+ *
+ * this is the longer one
+ * main is basically the entry point of our program
  */
 int main(void)
 {
 	char string[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(string);
+	/**
+	 * return - error code 0: no error
+	 *
+	 * this indicates that main() exited successfully
+	 */
 	return (0);
 }
