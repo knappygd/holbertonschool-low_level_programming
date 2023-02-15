195cc05396e880b7d4dcf747bb2210e3b11b3c32
Emilio Damasco <6338@holbertonstudents.com>
2023-02-15 09:15:24 -0800

    autocommit

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
