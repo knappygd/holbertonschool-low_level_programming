d71862e6b4398eb835def503034ec5d31e35ce36
Emilio Damasco <6338@holbertonstudents.com>
2023-02-15 09:55:44 -0800

    autocommit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		puts("%d is positive");
+		puts("%d is positive", n);
 	} 
 	else if (n < 0)
 	{
-		puts("%d is negative");
+		puts("%d is negative", n);
 	}
 	else
 	{
-		puts("%d is zero");
+		puts("%d is zero", n);
 	}
 	return (0);
 }
