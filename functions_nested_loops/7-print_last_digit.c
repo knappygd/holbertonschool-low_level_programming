458279c17b06fd2c7c15a84aba7395b6eb09e8c0
Emilio Damasco <6338@holbertonstudents.com>
2023-02-17 05:11:16 -0800

    autocommit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - entry, return the value of last digit
+ * @n: int to check
+ * @last: last digit of n
+ *
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	last = n % 10;
+	return (last);
+}
