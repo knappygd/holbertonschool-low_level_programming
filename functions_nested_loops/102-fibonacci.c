6ea54a93c01ab756e2e76a085d0866619d82f5f8
Emilio Damasco <6338@holbertonstudents.com>
2023-02-17 08:08:09 -0800

    autocommit

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print fibseq
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n1 = 0;
+	int n2 = 1;
+	int next = n1 + n2;
+
+	for (int i = 0; i <= 50; i++)
+	{
+		printf("%d, ", next);
+		printf("\n");
+
+		n1 = n2;
+		n2 = next;
+		next = n1 + n2;
+	}
+
+	return (0);
+}
