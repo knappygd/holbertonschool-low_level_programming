948752c3936cd46ea50e4b6488a5e477351aca06
Emilio Damasco <6338@holbertonstudents.com>
2023-02-17 06:28:15 -0800

    autocommit

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - print the 9 times table
+ *
+ */
+void times_table(int n)
+{
+	int num, times, res;
+
+	if (n <= 15 || n >= 0)
+	{
+
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (times = 1; times <= n; times++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				res = num * times;
+
+				if (res <= n)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((res / 10) + '0');
+				}
+
+				_putchar((res % 10) + '0');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
+
