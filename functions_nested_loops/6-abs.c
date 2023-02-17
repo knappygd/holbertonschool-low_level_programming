db70536d3210e2713ecd3455674c84811ab33ada
Emilio Damasco <6338@holbertonstudents.com>
2023-02-17 05:06:39 -0800

    autocommit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - entry point, evaluate absolute value of int
+ *
+ * @n: int to check
+ * Return: n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else 
+	{
+		return (-n);
+	}
+}
