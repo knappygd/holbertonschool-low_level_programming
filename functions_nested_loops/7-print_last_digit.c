fb201d36ec8b75acfff3f8ed358e31b75e1adfff
Emilio Damasco <6338@holbertonstudents.com>
2023-02-17 05:20:48 -0800

    autocommit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,5 +11,15 @@ int print_last_digit(int n)
 	int last;
 
 	last = n % 10;
-	return (last);
+
+	if (n > 0)
+	{
+		_putchar(last + 48);
+		return (last);
+	}
+	else
+	{
+		_putchar(-last + 48);
+		return (-last);
+	}
 }
