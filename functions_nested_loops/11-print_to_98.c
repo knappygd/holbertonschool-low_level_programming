d8fbd349d2693cfcc86d96793c5ee38dd61f6414
Emilio Damasco <6338@holbertonstudents.com>
2023-02-17 06:16:53 -0800

    autocommit

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 			{
 				printf("%d", n);
 				printf(", ");
-			} 
+			}
 			else
 			{
 				printf("%d", n);
@@ -28,19 +28,19 @@ void print_to_98(int n)
 		{
 			if (n != 98)
 			{
-                                printf("%d", n);
-                                printf(", ");;
+				printf("%d", n);
+				printf(", ");
 			}
-                        else
-                        {
-                                printf("%d", n);
-                        }
+			else
+			{
+				printf("%d", n);
+			}
 		}
 		printf("\n");
 	}
-	else 
+	else
 	{
 		printf("%d", n);
 		printf("\n");
-	}	
+	}
 }
