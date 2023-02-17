37afea2281cc6eb76ee03f252ff199cc49026d14
Emilio Damasco <6338@holbertonstudents.com>
2023-02-17 05:39:16 -0800

    autocommit

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * jack_bauer - print every min of the day
+ * 
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int hours;
+	int mins;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (mins = 0; mins <= 59; mins++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
