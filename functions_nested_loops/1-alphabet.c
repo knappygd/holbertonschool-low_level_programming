e18c17fddc3e8104b523f44bfa37189726674e8f
Emilio Damasco <6338@holbertonstudents.com>
2023-02-17 04:16:36 -0800

    autocommit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	retun (0);
+}
