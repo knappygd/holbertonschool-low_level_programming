7f65b0ff2dc2b1cea6466c8e9c2e00b90270e441
Emilio Damasco <6338@holbertonstudents.com>
2023-02-17 07:44:36 -0800

    autocommit

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print sum of multiples
+ *
+ * Return: sum
+ */
+int main(sum)
+{
+	int n;
+	int res;
+
+	for (n = 0; n < 1000; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			res += n;
+		}
+	}
+
+	return (sum);
+}
