c6e9957f39d260fdf67c14320307bb9ad09c6395
Emilio Damasco <6338@holbertonstudents.com>
2023-02-22 07:03:54 -0800

    autocommit

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers
+ *
+ * Return: idk
+ */
+void more_numbers(void)
+{
+	int i, n, x, c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 48; n < 58; n++)
+		{
+			_putchar(n);
+			
+			if (n >= 58)
+			{
+				for (x = 0; x < 5; x++)
+				{
+					for (c = 48; c < 54; c++)
+					{
+						_putchar(n + c);
+					}
+				}
+			}
+		}
+	}
+}
