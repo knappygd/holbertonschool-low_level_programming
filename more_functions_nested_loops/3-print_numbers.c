bee372d286cd5f6ff8458023bdf8c16511ffc937
Emilio Damasco <6338@holbertonstudents.com>
2023-02-22 06:40:39 -0800

    autocommit

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: idk
+ */
+void print_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
