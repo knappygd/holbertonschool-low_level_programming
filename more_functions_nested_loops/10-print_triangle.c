a22e561cf41b81a1249816b00af3284b94645fad
Emilio Damasco <6338@holbertonstudents.com>
2023-02-22 09:56:49 -0800

    autocommit

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 	{
 		for (height = 1; height < size + 1; height++)
 		{
-			for (spaces = 0; spaces < height - 1; spaces++)
+			for (spaces = size; spaces => 0; spaces--)
 			{
 				_putchar(' ');
 			}
