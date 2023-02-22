84c25ed94085fbc01a199d8026154e57552008c8
Emilio Damasco <6338@holbertonstudents.com>
2023-02-22 07:11:34 -0800

    autocommit

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,24 +7,16 @@
  */
 void more_numbers(void)
 {
-	int i, n, x, c;
+	int i, n;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (n = 48; n < 64; n++)
+		for (n = 0; n > 15; n++)
 		{
-			_putchar(n);
-			
-			if (n >= 58)
+			if (n > 9)
 			{
-				for (x = 0; x < 5; x++)
-				{
-					for (c = 48; c < 54; c++)
-					{
-						_putchar(n + c);
-					}
-				}
+				_putchar((n / 10) + '0');
 			}
-		}
+			_putchar((n % 10) + '0');
 	}
 }
