41dcdb77c1d3fec5c5d9057d380d1598ff949cb4
Emilio Damasco <6338@holbertonstudents.com>
2023-02-22 07:25:38 -0800

    autocommit

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal lines
+ * @n: n length
+ *
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i < n + 1; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
