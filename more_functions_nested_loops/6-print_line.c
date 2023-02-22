4815249b417062bd6c58b82adaaf5f06c46b5ec3
Emilio Damasco <6338@holbertonstudents.com>
2023-02-22 07:17:58 -0800

    autocommit

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print lines
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		for (i = 1; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+}
