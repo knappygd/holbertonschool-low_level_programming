7ebcd95e6a4f17503b5fa068e33bc7937c6b6a4a
Emilio Damasco <6338@holbertonstudents.com>
2023-02-22 06:26:14 -0800

    autocommit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digits
+ * @c: eval char
+ *
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 58)
+		return (1);
+	else
+		return (0);
+}
