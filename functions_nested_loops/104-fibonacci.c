440e05f3d9fb8455e553e2336ef51ca2b9f891b7
Emilio Damasco <6338@holbertonstudents.com>
2023-02-23 04:03:51 -0800

    autocommit

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	float n1 = 0, n2 = 1, next = n1 + n2;
+	unsigned long n1 = 0, n2 = 1, next = n1 + n2;
 	int i;
 
 	for (i = 0; i < 98; i++)
 	{
-		printf("%f", next);
+		printf("%ld", next);
 
 		n1 = n2;
 		n2 = next;
