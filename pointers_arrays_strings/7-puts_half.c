8745c9eec7189ba651d57c1df322e97a1b6821f4
Emilio Damasco <6338@holbertonstudents.com>
2023-02-27 10:28:20 -0800

    autocommit

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - print last half of chars
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int len = 0;
+	int half;
+
+	while (str[i++])
+		len++;
+
+	if (len % 2 == 0)
+		half = len / 2;
+	else 
+		half = (len - 1) / 2;
+
+	for (i = half; i <= len; i++)
+		_putchar(str[i]);
+}
+	
