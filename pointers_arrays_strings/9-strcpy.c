119e8d8ba22f4a8c21a9d61925ae2a23bcf9ca48
Emilio Damasco <6338@holbertonstudents.com>
2023-02-27 11:11:30 -0800

    autocommit

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,12 +4,14 @@
  * _strcpy - copy a string
  * @dest: destination buffer
  * @src: strig to copy
+ *
+ * Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i])
+	while (src[i] + 1)
 	{
 		dest[i] = src[i];
 		i++;
