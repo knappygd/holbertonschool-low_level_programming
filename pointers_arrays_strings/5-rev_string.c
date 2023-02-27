7d5c14b9bf19dcc7f81b47e55b2f562f1d7b1330
Emilio Damasco <6338@holbertonstudents.com>
2023-02-27 09:44:39 -0800

    autocommit

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverse a string
+ */
+void rev_string(char *s)
+{
+	int i, len = strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
