20951c3f0574467053bad7fd41f8f47ab5e4c278
Emilio Damasco <6338@holbertonstudents.com>
2023-02-27 10:44:57 -0800

    autocommit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print an array
+ * @a: array
+ * @n: times
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i == n - 1)
+			continue;
+
+		printf(", ");
+	}
+}
