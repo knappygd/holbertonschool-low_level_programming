93ed84d0a834d263a1e4b664d21763d36c9a86d7
Emilio Damasco <6338@holbertonstudents.com>
2023-02-27 09:56:46 -0800

    autocommit

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,12 +7,15 @@ void rev_string(char *s)
 {
 	int i;
 	int len = 0;
+	char aux;
 
 	while (s[i++])
 		len++;
 
 	for (i = len - 1; i >= len / 2; i--)
 	{
-		_putchar(s[i]);
+		aux = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = aux;
 	}
 }
