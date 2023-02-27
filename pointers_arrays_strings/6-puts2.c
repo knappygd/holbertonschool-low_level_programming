804442e578fec6e6720266ea5beff05e9d48ab7c
Emilio Damasco <6338@holbertonstudents.com>
2023-02-27 10:13:18 -0800

    autocommit

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - print a string
+ * @str: pointer
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int len = 0;
+	
+	while (str[i++])
+		len++;
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
