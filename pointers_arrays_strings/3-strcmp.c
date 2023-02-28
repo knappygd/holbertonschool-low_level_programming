a573527b1423414fd5626d9054625a07db25b0c4
Emilio Damasco <6338@holbertonstudents.com>
2023-02-28 08:01:06 -0800

    autocommit

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, n = 0;
+	int i;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
-			n = s1[i] - s2[i];
+			return (s1[i] - s2[i]);
 	}
 
-	return (n);
+	return (0);
 }
