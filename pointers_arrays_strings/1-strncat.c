34151b318df77a86fddac52183b33e16689633d9
Emilio Damasco <6338@holbertonstudents.com>
2023-02-28 06:22:54 -0800

    autocommit

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate strings n limit
+ * @dest: str1
+ * @src: str2
+ * @n: limit
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
