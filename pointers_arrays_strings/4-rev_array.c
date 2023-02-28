d29d451da0b7c2ef2b3ea2b28636c3a1aa9e83d0
Emilio Damasco <6338@holbertonstudents.com>
2023-02-28 08:31:36 -0800

    autocommit

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - reverse an array
+ * @a: array
+ * @n: elements to swap
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, aux;
+
+	for (i = n - 1; i >= n / 2; i--)
+	{
+		aux = a[i];
+		a[i] = a[n - i - 1];
+		a[n -i -1] = aux;
+	}
+}
