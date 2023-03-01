15ac502642b475728cea37cb54b0fea812e613e5
Emilio Damasco <6338@holbertonstudents.com>
2023-03-01 05:56:04 -0800

    autocommit

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strchr - smae as strchr
+ * @s: string to scan
+ * @c: chat to locate
+ *
+ * Return: pointer to the first occurence of c in s
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			s += i;
+		}
+	}
+
+	return (s);
+}
