b88d852c6e52ce6ec53ecfa20899ad3aa0890fcf
Emilio Damasco <6338@holbertonstudents.com>
2023-03-01 04:06:35 -0800

    autocommit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n)
+	while (*src++)
 	{
-		dest[i] = src[i];
+		if (i < n)
+		{
+			dest[i] = src[i];
+		}
+
 		i++;
 	}
 
