58dcf6c4ce44382a968b9390ce07b0bf3f2eb5fb
Emilio Damasco <6338@holbertonstudents.com>
2023-03-02 04:31:05 -0800

    autocommit

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (i = 0; needle[i]; i++)
 		{
-			if (haystack[i] == needle)
+			if (*needle == haystack[i])
 			{
 				return (haystack);
 			}
