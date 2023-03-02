0cefc11f41e3267d08f2d290abf635c550d47d95
Emilio Damasco <6338@holbertonstudents.com>
2023-03-02 04:13:13 -0800

    autocommit

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -12,9 +12,9 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 
-	while (*accept)
+	while (*s)
 	{
-		for (i = 0; s[i]; i++)
+		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
 			{
@@ -23,7 +23,7 @@ char *_strpbrk(char *s, char *accept)
 
 		}
 
-		accept++;
+		s++;
 	}
 
 	return (0);
