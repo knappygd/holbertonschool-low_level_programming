bc43d436738c5a09f7c6dc1ec43ab569397754d4
Emilio Damasco <6338@holbertonstudents.com>
2023-03-06 06:05:22 -0800

    autocommit

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - strlen
+ * @s: string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return(1 + _strlen_recursion(s + 1));
+	}
+
+	return (0);
+}
