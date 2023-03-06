86c620fca06e581b6ee68a65ecc442b515c6fef3
Emilio Damasco <6338@holbertonstudents.com>
2023-03-06 10:05:49 -0800

    autocommit

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <math.h>
+/**
+ * _sqrt_recursion - sqrt
+ * @n: sqrt num
+ *
+ * Return: result
+ */
+int _sqrt_recursion(int n)
+{
+	float p = 1 / 2;
+	int res = pow(n, p);
+	
+	return (res);
+}
