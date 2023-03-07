24ded226cbc83f8755db75dd5e3752b214f688a4
Emilio Damasco <6338@holbertonstudents.com>
2023-03-07 10:31:37 -0800

    autocommit

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two numbers
+ * @argc: count
+ * @argv: args entered
+ *
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int res;
+	char *p;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int fconv = strtol(argv[1], &p, 10);
+		int sconv = strtol(argv[2], &p, 10);
+		
+		res = fconv * sconv;
+
+		printf("%d\n", res);
+	}
+
+	return (0);
+}
