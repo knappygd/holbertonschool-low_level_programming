7fa4c771ea7eb2d9965b10e659e4074b55b59474
Emilio Damasco <6338@holbertonstudents.com>
2023-03-07 09:52:41 -0800

    autocommit

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print file name
+ * @argc: count
+ * @argv: arg vectors
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", argv[argc - 1]);
+
+	return (0);
+}
