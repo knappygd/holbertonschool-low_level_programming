a0261aa46c45badcd6369f3e6fe5673d50b3693c
Emilio Damasco <6338@holbertonstudents.com>
2023-03-13 09:29:35 -0700

    autocommit

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create an array
+ * @size: array size
+ * @c: content
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	free(arr);
+
+	return (arr);
+}
