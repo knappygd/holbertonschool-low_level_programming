df1bd74efab760e3049e215eac64f8c05a8c65c3
Emilio Damasco <6338@holbertonstudents.com>
2023-03-13 11:52:54 -0700

    autocommit

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,18 +16,18 @@ char *str_concat(char *s1, char *s2)
 	int len2 = strlen(s2);
 	char *conc;
 
-	if (s1 == NULL)
-		/*s1 = "";*/
-		conc = s2;
-		return (conc);
-
-	if (s2 == NULL)
-		s2 = "";
 
 	conc = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (conc == NULL)
 		return (NULL);
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+	
 
 	for (i = 0; i < len1; i++)
 	{
