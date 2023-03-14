a8bb70e216131e2a6f1961a458c0361edc021493
Emilio Damasco <6338@holbertonstudents.com>
2023-03-14 09:21:38 -0700

    autocommit

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,8 +16,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *conc;
 	unsigned int i, len, len2;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	len = strlen(s1);
 	len2 = strlen(s2);
