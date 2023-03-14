24d8afe3c02a8fafbfca597e897c1cadb495af62
Emilio Damasco <6338@holbertonstudents.com>
2023-03-14 09:53:54 -0700

    autocommit

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -12,13 +12,14 @@ int *array_range(int min, int max)
 	int *array;
 	int i;
 
+	if (min > max)
+		return (NULL);
+
 	array = malloc(sizeof(int) * (max - min + 1));
 
 	if (array == NULL)
 		return (NULL);
 
-	if (min > max)
-		return (NULL);
 
 	for (i = min; i <= max; i++)
 		array[i - min] = i;
