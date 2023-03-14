91b6289849a16438232d398e7062089c66eaf8d3
Emilio Damasco <6338@holbertonstudents.com>
2023-03-14 06:56:01 -0700

    autocommit

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -22,12 +22,8 @@ char *str_concat(char *s1, char *s2)
 	if (conc == NULL)
 		return (NULL);
 
-	if (s1 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
 
 	for (i = 0; i < len1; i++)
 	{
