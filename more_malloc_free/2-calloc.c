8c9831c2e248c8249b0acc7e83117d8f1e6cd31d
Emilio Damasco <6338@holbertonstudents.com>
2023-03-14 07:23:56 -0700

    autocommit

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,16 +10,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *array;
+	int i;
 
 	array = malloc(nmemb * size);
-
-	array = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	if (array == NULL)
 		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		array[i] = 0;
 
 	return (array);
 }
