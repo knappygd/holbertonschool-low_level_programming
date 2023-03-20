a2672f51860124e7ea343937323450b937249904
Emilio Damasco <6338@holbertonstudents.com>
2023-03-20 10:15:23 -0700

    autocommit

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,14 +10,14 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name != NULL)
-			printf("Name: %s", d->name);
+			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)");
 
-		printf("Age: %f", d->age);
+		printf("Age: %f\n", d->age);
 
 		if (d->owner != NULL)
-			printf("Owner: %s", d->owner);
+			printf("Owner: %s\n", d->owner);
 		else
 			printf("Owner: (nil)");
 	}
