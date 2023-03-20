d47ae0ccb3423416f34af26ffabdbcb0f009cf7c
Emilio Damasco <6338@holbertonstudents.com>
2023-03-20 11:45:51 -0700

    autocommit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,6 +16,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
+	dog->name = malloc(sizeof(name));
+	dog->owner = malloc(sizeof(owner));
+
 	dog->name = strcpy(dog->name, name);
 	dog->age = age;
 	dog->owner = strcpy(dog->owner, owner);
