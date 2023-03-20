61ef4252d38c94b60057fc1456e98de8367a9923
Emilio Damasco <6338@holbertonstudents.com>
2023-03-20 08:45:50 -0700

    autocommit

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#define DOG
+#ifndef DOG
+
+/**
+ * struct dog - dog type struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *onwer;
+};
+
+#endif
