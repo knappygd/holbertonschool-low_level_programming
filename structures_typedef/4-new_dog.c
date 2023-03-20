711328351e607faa35ad587e489b63dfbf984805
Emilio Damasco <6338@holbertonstudents.com>
2023-03-20 11:42:03 -0700

    autocommit

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - a new dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dow owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = strcpy(dog->name, name);
+	dog->age = age;
+	dog->owner = strcpy(dog->owner, owner);
+}
