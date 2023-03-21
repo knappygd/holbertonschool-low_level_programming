b9f4e84c11554d1e4b04565d51781eb6b8e0d2af
Emilio Damasco <6338@holbertonstudents.com>
2023-03-21 05:43:08 -0700

    autocommit

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name
+ * @name: name to print
+ * @f: function to print the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
