d88f7f5c7146b0ffdb3c736a3e52b2d651951f66
Emilio Damasco <6338@holbertonstudents.com>
2023-03-22 10:04:50 -0700

    autocommit

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,12 +10,12 @@
 int sum_them_all(const unsigned int n,...)
 {
 	va_list vl;
-	const unsigned int i;
+	unsigned int i;
 	int sum = 0;
 
 	va_start(vl, n);
 
-	for (i = 0; i < count; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(vl, const unsigned int);
 
 	va_end(vl);
