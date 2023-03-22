ce9a782188507e8b95bef10bcac473cf02862314
Emilio Damasco <6338@holbertonstudents.com>
2023-03-22 09:56:33 -0700

    autocommit

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,6 +1,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * sum_them_all - sum numbers
+ * @n: the numbers
+ *
+ * Return: sum
+ */
 int sum_them_all(const unsigned int n,...)
 {
 	va_list ap;
