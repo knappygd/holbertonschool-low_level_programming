f3aee7388856497e9f06891ad12c3ffd55dc4d2a
Emilio Damasco <6338@holbertonstudents.com>
2023-03-23 05:25:03 -0700

    autocommit

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(vl, const unsigned int));
-		if (n != i - 1)
+		if (n != i - 2)
 			printf("%s", separator);
 	}
 
