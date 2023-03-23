f54c150b7c9654b78de71f008704e08ccff05ac2
Emilio Damasco <6338@holbertonstudents.com>
2023-03-23 05:42:41 -0700

    autocommit

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(vl, char *);
-		
+
 		if (string == NULL)
 			string = "(nil)";
 
