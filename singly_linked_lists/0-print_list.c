1bede352df3e653ea919b58d9a0a1037abb39df3
Emilio Damasco <6338@holbertonstudents.com>
2023-04-03 10:02:03 -0700

    autocommit

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,14 +14,13 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
-		else
-			printf("[%u] %s", h->len, h->str);
+			printf("[0] (nil)\n");
+		
+		printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
 		n++;
 	}
-
 
 	return (n);
 }
