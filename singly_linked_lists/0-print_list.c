c45f796aaf6f3dc4398cb6bbc037baeb19d10026
Emilio Damasco <6338@holbertonstudents.com>
2023-04-03 09:43:06 -0700

    autocommit

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_list - prints all elelments if a list_t list
+ * @h: struct
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%u] %s", h->str, h->len);
+
+		n++;
+	}
+
+
+	return (n);
+}
