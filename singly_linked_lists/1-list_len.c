3a949a5d4456a70bf5493e7f8596eaf23804decd
Emilio Damasco <6338@holbertonstudents.com>
2023-04-03 11:45:20 -0700

    autocommit

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_len - returns the number of elements in a linked list
+ * @h: struct
+ *
+ * Return: number of nodes
+ */
+size_t print_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
