b587f31a69da497517ec9bfff8ab9831e9e9633e
Emilio Damasco <6338@holbertonstudents.com>
2023-04-04 16:17:52 -0700

    autocommit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,12 +10,11 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *buffer, *new;
+	dlistint_t *buffer = *h;
+	dlistint_t *new;
 
 	if (idx == 0)
 		new = add_dnodeint(h, n);
-
-	buffer = *h;
 
 	while (idx != 1)
 	{
