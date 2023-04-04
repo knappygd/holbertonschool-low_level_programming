2da140b79b455c4043e58776442008fd6159843b
Emilio Damasco <6338@holbertonstudents.com>
2023-04-04 16:43:45 -0700

    autocommit

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete a node at index
+ * @head: list head
+ * @index: node to delete
+ *
+ * Return: 1 if succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *buffer;
+
+	if (!*head)
+		return (-1);
+
+	buffer = *head;
+
+	while (index != 0)
+	{
+		buffer = buffer->next;
+
+		if (!buffer)
+			return (-1);
+
+		index--;
+	}
+
+	if (buffer == *head)
+	{
+		*head = buffer->next;
+
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		buffer->prev->next = buffer->next;
+
+		if (buffer->next)
+			buffer->next->prev = buffer->prev;
+	}
+
+	return (1);
+}
