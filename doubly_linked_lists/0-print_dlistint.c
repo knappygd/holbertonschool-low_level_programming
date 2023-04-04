2851d3ebaabbbfb8c685ec3a7cf3a625dcafaec0
Emilio Damasco <6338@holbertonstudents.com>
2023-04-04 05:56:00 -0700

    autocommit

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints a dlistint list
+ * @h: header
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		printf("%u\n", h->len);
+
+		h = h->next;
+
+		nodes++;
+	}	
+
+	return (nodes);
+}
