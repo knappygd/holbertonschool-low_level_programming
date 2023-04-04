09246f69682b32c42295ad03cc0e14fb8fb06517
Emilio Damasco <6338@holbertonstudents.com>
2023-04-04 06:16:51 -0700

    autocommit

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: list head node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *buffer;
+
+	while (head != NULL)
+	{
+		buffer = head;
+		head = head->next;
+		free(buffer->n);
+		free(buffer);
+	}
+
+	free(head);
+}
