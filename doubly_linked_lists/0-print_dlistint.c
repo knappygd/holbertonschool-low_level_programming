dd79d7096a496adf19361e2762fc0ea840846b8c
Emilio Damasco <6338@holbertonstudents.com>
2023-04-04 14:19:23 -0700

    autocommit

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -6,7 +6,7 @@
  *
  * Return: number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+/*size_t print_dlistint(const dlistint_t *h)
 {
 	int nodes = 0;
 
@@ -18,4 +18,22 @@ size_t print_dlistint(const dlistint_t *h)
 	}
 
 	return (nodes);
+}*/
+size_t print_dlistint(const dlistint_t *h)
+{
+    int nodes = 0;
+
+    while (h)
+    {
+        printf("Node value: %u\n", h->n);
+        printf("Prev node address: %p\n", (void *) h->prev);
+        printf("Next node address: %p\n", (void *) h->next);
+
+        nodes++;
+        h = h->next;
+    }
+
+    printf("Total number of nodes: %d\n", nodes);
+    return (nodes);
 }
+
---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - return the nth node of a dlistint_t list
+ * @head: head
+ * @index: node index
+ *
+ * Return: node
+ */
