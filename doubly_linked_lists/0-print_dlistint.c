091338c5b6fcbb27e64c77c47dc42e83ac25e3d2
Emilio Damasco <6338@holbertonstudents.com>
2023-04-04 14:23:11 -0700

    autocommit

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -6,31 +6,17 @@
  *
  * Return: number of nodes
  */
-/*size_t print_dlistint(const dlistint_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
 	int nodes = 0;
 
 	while (h)
 	{
-		printf("%u\n", h->n);
+		printf("%d\n", h->n);
 		nodes++;
 		h = h->next;
 	}
 
 	return (nodes);
-}*/
-size_t print_dlistint(const dlistint_t *h)
-{
-    int nodes = 0;
-
-    while (h)
-    {
-        printf("%u\n", h->n);
-
-        nodes++;
-        h = h->next;
-    }
-
-    return (nodes);
 }
 
