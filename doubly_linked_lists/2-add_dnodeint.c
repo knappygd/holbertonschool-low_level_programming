de995d94845389dc8949623396af61ee1dfb07de
Emilio Damasco <6338@holbertonstudents.com>
2023-04-04 14:41:59 -0700

    autocommit

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (!new)
 	{
 		free(new);
 		return (NULL);
@@ -22,6 +22,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
+
+	if (*head)
+		(*head)->prev = new;
+	
 	*head = new;
 
 	return (new);
