aa31da038f2ca442295768ac5037c6a28b82c12c
Emilio Damasco <6338@holbertonstudents.com>
2023-04-07 05:48:12 -0700

    autocommit

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_binary - print the binary representation of a number
+ * @n: number to convert
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int power = 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else if (n == 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		while (power <= n)
+		{
+			power *= 2;
+		}
+
+		power /= 2;
+
+		while (power > 0)
+		{
+			if (n >= power)
+			{
+				_putchar('1');
+				n -= power;
+			}
+			else
+			{
+				_putchar('0');
+			}
+
+			power /= 2;
+		}
+	}
+}
