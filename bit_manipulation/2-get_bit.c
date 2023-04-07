cc984f67f4da9864a4c8127ab5b1c9eae1dc874e
Emilio Damasco <6338@holbertonstudents.com>
2023-04-07 07:52:12 -0700

    autocommit

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,64 +1,4 @@
 #include "main.h"
-#include <string.h>
-#include <limits.h>
-#include <stdlib.h>
-
-/**
- * _print_binary - transform to binary
- * @n: decimal
- *
- * Return: bin string
- */
-char *_print_binary(unsigned long int n)
-{
-	unsigned long int power = 1;
-	int i;
-	char *bin;
-	char append;
-
-	bin = (char *)malloc(sizeof(power));
-	if (n == 0)
-	{
-		return ("0");
-	}
-	else if (n == 1)
-	{
-		return ("1");
-	}
-	else if (n == ULONG_MAX)
-	{
-		append = '1';
-		while (i < 64)
-		{
-			strncat(bin, &append, 1);
-			i++;
-		}
-	}
-	else
-	{
-		while (power <= n)
-		{
-			power *= 2;
-		}
-		power = power >> 1;
-		while (power > 0)
-		{
-			if (n >= power)
-			{
-				append = '1';
-				strncat(bin, &append, 1);
-				n -= power;
-			}
-			else
-			{
-				append = '0';
-				strncat(bin, &append, 1);
-			}
-			power = power >> 1;
-		}
-	}
-	return (bin);
-}
 
 /**
  * get_bit - returns the value of a bit at a given index
@@ -71,16 +11,18 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
-	_print_binary(n);
+	if (n == 0)
+		return (0);
 
-	while (_print_binary(n)[i])
+	while (i < 64)
 	{
 		if (i == index)
 		{
-			return(_print_binary(n)[i]);
+			return(n & 1);
 		}
 
 		i++;
+		n >>= 1;
 	}
 
 	return (-1);
