9b075f3f7ca247169cfc35872e777b168b98f2ff
Emilio Damasco <6338@holbertonstudents.com>
2023-04-06 17:15:14 -0700

    autocommit

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert a binary number into a uint
+ * @b: 1s and 0s string
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int i, multiplier = 1;
+
+	if (!b)
+		return (0);
+
+	while (b)
+	{
+		if (b[i] != '0' || b[i] != '1')
+			return (0);
+		i++;
+	}
+
+	while (b)
+	{
+		if (b[i] == 1)
+		{
+			num += multiplier;		
+		}
+
+		multiplier *= 2;
+	}
+
+	return (num);
+}
+
