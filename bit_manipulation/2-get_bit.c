b68748ba78262443115f888b74efaa8869218084
Emilio Damasco <6338@holbertonstudents.com>
2023-04-07 06:15:51 -0700

    autocommit

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,81 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number
+ * @index: which digit to check
+ *
+ * Return: value of a bit
+ */
+char *_print_binary(unsigned long int n)
+{
+	unsigned long int power = 1;
+	int i;
+	char *bin;
+	char append;
+
+	bin = (char *)malloc(sizeof(power));
+	if (n == 0)
+	{
+		return ("0");
+	}
+	else if (n == 1)
+	{
+		return ("1");
+	}
+	else if (n == ULONG_MAX)
+	{
+		append = '1';
+		while (i < 64)
+		{
+			strncat(bin, &append, 1);
+			i++;
+		}
+	}
+	else
+	{
+		while (power <= n)
+		{
+			power *= 2;
+		}
+		power = power >> 1;
+		while (power > 0)
+		{
+			if (n >= power)
+			{
+				append = '1';
+				strncat(bin, &append, 1);
+				n -= power;
+			}
+			else
+			{
+				append = '0';
+				strncat(bin, &append, 1);
+			}
+			power = power >> 1;
+		}
+	}
+	return (bin);
+}
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i;
+
+	_print_binary(n);
+
+	while (_print_binary(n)[i])
+	{
+		if (i == index)
+		{
+			return(_print_binary(n)[i]);
+		}
+		else
+		{
+			return (-1);
+		}
+
+		i++;
+	}
+}
