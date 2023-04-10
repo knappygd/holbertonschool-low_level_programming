efaaad77444d948a806172d54a542064b935e6df
Emilio Damasco <6338@holbertonstudents.com>
2023-04-10 15:53:38 -0700

    autocommit

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to flip
+ * to get from one number to another
+ * @n: original number
+ * @m: flipped number
+ *
+ * Return: number of bits needed to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int xor = n ^ m, nbits = 0;
+
+	while (n)
+	{
+		n &= (n - 1);
+		nbits++;
+	}
+
+	return (nbits);
+}
