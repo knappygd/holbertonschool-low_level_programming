80286001c4a4af1c1f16c7b7a91156e6aa55b442
Emilio Damasco <6338@holbertonstudents.com>
2023-04-12 09:41:10 -0700

    autocommit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,6 @@
 int create_file(const char *filename, char *text_content)
 {
 	int op;
-	char *buffer = malloc(sizeof(text_content));
 
 	if (!filename)
 		return (-1);
@@ -23,10 +22,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	write(op, buffer, strlen(text_content));
+	write(op, text_content, strlen(text_content));
 
 	close(op);
-	free(buffer);
 
 	return (1);
 }
