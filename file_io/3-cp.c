6c27e7b30a28f02f20a14771d51a0611425333b2
Emilio Damasco <6338@holbertonstudents.com>
2023-04-12 12:05:49 -0700

    autocommit

---
+++ file_io/3-cp.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - copy the contents of one file into another
+ * @argc: number of arguments passed
+ * @argv: an array of the arguments passed
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int from, to, rd, wr, cf, ct;
+	char *buffer;
+	char *file_from = argv[1];
+	char *file_to = argv[2]
+
+	if (argc != 3)
+	{
+		printf("Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (!buffer)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	from = open(file_from, O_RDONLY);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	rd = read(from, buffer, 1024);
+	to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	wr = write(to, buffer, 1024);
+
+	free(buffer);
+	cf = close(from);
+	if (cf == -1)
+	{
+		dprintf("Error: Can't close fd FD_VALUE\n");
+		exit(100);
+	}
+	ct = close(to);
+	if (ct == -1)
+	{
+		dprintf("Error: Can't close fd FD_VALUE\n");
+		exit(100);
+	}
+
+	return (0);
+}
