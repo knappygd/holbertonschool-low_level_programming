e0a6fe21f67852c60a8b8d2eb2cd214bd216239a
Emilio Damasco <6338@holbertonstudents.com>
2023-04-12 09:30:58 -0700

    autocommit

---
+++ file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ * @filename: file to use
+ * @text_content - text to write to the file
+ *
+ * Return: 1 on success, else -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int op, rd, wr;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+
+	if (!text_content)
+		text_content = "";
+
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	close(op);
+}
