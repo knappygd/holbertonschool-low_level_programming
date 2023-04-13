313468b5c4814af795b17685d3db771a3efd1c92
Emilio Damasco <6338@holbertonstudents.com>
2023-04-13 06:25:48 -0700

    autocommit

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,19 +10,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *htable;
-	hash_node_t **node;
 
 	htable = malloc(sizeof(hash_table_t));
 	if (!htable)
 		return (NULL);
 
-	node = malloc(sizeof(hash_node_t *) * size);
-	if (!node)
+	htable->array = malloc(sizeof(hash_node_t *) * size);
+	if (!htable->array)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
-		node[i] = NULL;
+		htable->array[i] = NULL;
 	}
 
 	htable->size = size;
